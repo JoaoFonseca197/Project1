09e439ee07ff46f1e2e373d8bf89cde03bcb0d96
António Branco <ajsbranco15@gmail.com>
2020-04-17 19:06:03 +0100

    Board Drawing Method

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -6,14 +6,39 @@ namespace Project1
     {
         static void Main(string[] args)
         {
-            //variáveis
-            int[,] matriz = new int [8,8]
-            //fazer tabuleiro
+            //variables
+            int[,] matriz = new int[8, 8];
+
             //fazer lobo
             //fazer ovelhas
             //pedir input do jogador lobo
             //pedir input do jogador ovelha
+            board();
+        }
+        // Draws the Board
+        static void board() 
+        {
+            string horizontalbar = "---------------------------------";
+            string verticalbar = "| ";
+            string Numbers = ("1 2 3 4 5 6 7 8");
+            string letters = ("A B C D E F G H");
+            int lins = 8, cols = 8, i, j;
+            int[,] m = {  {1,2,3,4,5,6,7,8},
+                                            {1,2,3,4,5,6,7,8},
+                                            {1,2,3,4,5,6,7,8},
+                                            {1,2,3,4,5,6,7,8},
+                                            {1,2,3,4,5,6,7,8},
+                                            {1,2,3,4,5,6,7,8},
+                                            {1,2,3,4,5,6,7,8},
+                                            {1,2,3,4,5,6,7,8}, };
 
+            for (i = 0; i < lins; i++)
+            {
+                Console.WriteLine(horizontalbar);
+                for (j = 0; j < cols; j++)
+                    Console.Write(verticalbar + "{0} ", m[i, j]);
+                Console.WriteLine(verticalbar);
+            }
         }
     }
 }

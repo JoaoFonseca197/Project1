22965dd5ac2f05643a832d7ec80f87be51de2362
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-04-17 11:34:48 +0100

    Matriz feita

---
+++ Project1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variáveis
+            int[,] matriz = new int [8,8]
+            //fazer tabuleiro
+            //fazer lobo
+            //fazer ovelhas
+            //pedir input do jogador lobo
+            //pedir input do jogador ovelha
+
+        }
+    }
+}

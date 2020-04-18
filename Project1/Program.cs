cea2710567eaab8a68b1ee08444a662dc66e7d93
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-04-18 14:29:00 +0100

    Inicial Wolf Position

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace Project1
 {
     class Program
@@ -7,18 +8,28 @@ namespace Project1
         static void Main(string[] args)
         {
             //variables
-
-
-            board();
+            Random rnd = new Random ();
+            int[] x_values = new int[4] {1,3,5,7};
+            int x = rnd.Next(x_values.Length);
+            int X = x_values[x];
+            int W_y = 0;
+            int W_x = X;
+            board(W_y,W_x);
+            Wolf();
+            Console.Write(X);
+            
         }
         // Draws the Board
-        static void board() 
+        private static void board(int Wy , int Wx) 
         {
             string [,] matrix;
             int lins = 8, cols = 8;
 
             //Inicialize the matrix
             matrix = new string [8,8];
+
+            //show position of wolf
+            matrix[Wy,Wx] = "W";
 
             //Fill the matrix with empty spaces
             for (int i = 0; i < lins; i++)
@@ -29,6 +40,9 @@ namespace Project1
                 }
             }
 
+            matrix[Wy,Wx] = "W";
+
+            
             // show the matrix
             for (int i = 0; i < lins; i++)
             {
@@ -40,6 +54,21 @@ namespace Project1
             }
             Console.WriteLine("  -------------------------");
             Console.WriteLine("    A   B   C   D   E   F  ");
+        }
+         private static void Wolf ()
+        {
+            //variables
+            /*string Wolf = "W";
+            Random rnd = new Random ();
+            int[] x_values = new int[4] {1,3,5,7};
+            int X = rnd.Next(0, x_values.Length);
+            Console.Write(x_values[X]);
+            //spawn wolf
+            current_pos = matrix[1,X];
+            //ask input
+            //move wolf
+            //detect ships
+            //detect end*/
         }
     }
 }
